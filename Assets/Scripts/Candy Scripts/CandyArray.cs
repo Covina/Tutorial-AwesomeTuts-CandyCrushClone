21132edabb7126da381d2394ca25f500817b1e5e
Covina <jabs303@gmail.com>
2017-10-15 21:32:37 -0700

    Section 7, Lecture 75: Candy Array and Candy classes; Swap functionality

---
+++ Assets/Scripts/Candy Scripts/CandyArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CandyArray {
+
+    // Crate the two-dimensional array
+    GameObject[,] candies = new GameObject[GameVariables.Rows, GameVariables.Columns];
+
+    // Copies of the two beans for swapping
+    private GameObject backup1;
+    private GameObject backup2;
+
+    // INDEXER - Get/Set the item at this row and column
+    public GameObject this[int row, int column]
+    {
+        get
+        {
+            try
+            {
+                return candies[row, column];
+            } catch(Exception e)
+            {
+                throw;
+            }
+        }
+
+        set
+        {
+            candies[row, column] = value;
+        }
+
+    }
+
+    /// <summary>
+    /// Swap function to change array information and positions of two candies
+    /// </summary>
+    /// <param name="g1"></param>
+    /// <param name="g2"></param>
+    public void Swap(GameObject g1, GameObject g2)
+    {
+        // Store backups
+        backup1 = g1;
+        backup1 = g2;
+
+        var g1Candy = g1.GetComponent<Candy>();
+
+        var g2Candy = g2.GetComponent<Candy>();
+
+        // store row/column data
+        int g1Row = g1Candy.Row;
+        int g1Column = g1Candy.Column;
+
+        // store row/column data
+        int g2Row = g2Candy.Row;
+        int g2Column = g2Candy.Column;
+
+
+
+        // Get the game object stored in position
+        var temp = candies[g1Row, g1Column];
+
+        // Swap 2 into position 1
+        candies[g1Row, g1Column] = candies[g2Row, g2Column];
+
+        // Swap 1 into position 2
+        candies[g2Row, g2Column] = temp;
+
+        // Now make the game object swap
+        Candy.SwapRowColumn(g1Candy, g2Candy);
+
+
+    }
+
+
+    /// <summary>
+    /// Undo the swap if necessary
+    /// </summary>
+    public void UndoSwap()
+    {
+        // use the same swap function above
+        Swap(backup1, backup2);
+
+    }
+
+
+}
---
+++ Assets/Scripts/Match Checkers/MatchChecker.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchChecker {
+
+    /// <summary>
+    /// Fade the opacity in/out to show a potential match
+    /// </summary>
+    /// <param name="potentialMatches"></param>
+    /// <returns></returns>
+    public static IEnumerator AnimatePotentialMatches(IEnumerable<GameObject> potentialMatches)
+    {
+
+        // loop through to reduce alpha from 1.0 to 0.3 as part of the animation
+        for (float i = 1.0f; i >= 0.3f; i -= 0.1f) {
+
+            // for each of the beans
+            foreach(var item in potentialMatches) {
+
+                // get the color
+                Color c = item.GetComponent<SpriteRenderer>().color;
+
+                // update the alpha value
+                c.a = i;
+
+                // set new alpha value
+                item.GetComponent<SpriteRenderer>().color = c;
+
+            }
+
+            // Length to wait for the opacity animation
+            yield return new WaitForSeconds(GameVariables.OpactiyAnimationDelay);
+
+        }
+
+
+        // loop through to increase alpha from 0.3 to 1.0 as part of the animation
+        for (float i = 0.3f; i <= 1.0f; i += 0.1f)
+        {
+
+            // for each of the beans
+            foreach (var item in potentialMatches)
+            {
+
+                // get the color
+                Color c = item.GetComponent<SpriteRenderer>().color;
+
+                // update the alpha value
+                c.a = i;
+
+                // set new alpha value
+                item.GetComponent<SpriteRenderer>().color = c;
+
+            }
+
+            // Length to wait for the opacity animation
+            yield return new WaitForSeconds(GameVariables.OpactiyAnimationDelay);
+
+        }
+
+    } // AnimatePotentialMatches()
+
+
+    /// <summary>
+    /// Checks if the two candies are next to each other.
+    /// </summary>
+    /// <param name="c1"></param>
+    /// <param name="c2"></param>
+    /// <returns></returns>
+    public static bool AreHorizontalOrVericalNeighbors(Candy c1, Candy c2)
+    {
+
+        // Check if the two candies are 1) In the same row or column and 2) Are adjacent to each other
+        return (c1.Column == c2.Column || c1.Row == c2.Row) && Mathf.Abs(c1.Column - c2.Column) <= 1 && Mathf.Abs(c1.Row - c2.Row) <= 1;
+
+    }
+
+
+
+
+
+}
